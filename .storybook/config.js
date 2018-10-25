f408811ed546aeba371422a4896561baf217c418
Merlin Patterson <merlinpatt@gmail.com>
2018-10-25 09:05:46 -0400

    Add storyshots addon

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,6 +1,10 @@
 import { configure, addDecorator } from '@storybook/react';
-import { withKnobs } from '@storybook/addon-knobs/react';
-import { setOptions } from '@storybook/addon-options';
+import { withKnobs } from '@storybook/addon-knobs';
+import { withOptions } from '@storybook/addon-options';
+
+if (process.env.NODE_ENV === 'test') {
+  require('babel-plugin-require-context-hook/register')();
+}
 
 const req = require.context('../src', true, /stories\.js$/);
 
@@ -10,9 +14,9 @@ function loadStories() {
 
 addDecorator(withKnobs);
 
-setOptions({
+addDecorator(withOptions({
   // name to display in the top left corner
-  name: 'Pup - Clever Beagle',
+  name: 'Fur - Clever Beagle',
   // URL for name in top left corner to link to
   url: 'https://cleverbeagle.com',
   // show story component as full screen
@@ -37,6 +41,6 @@ setOptions({
   sidebarAnimations: true,
   // id to select an addon panel
   selectedAddonPanel: undefined, // The order of addons in the "Addon panel" is the same as you import them in 'addons.js'. The first panel will be opened by default as you run Storybook
-});
+}));
 
 configure(loadStories, module);
