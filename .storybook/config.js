5b137276db8445e0cc63d2a279472d58e15b7fc8
Merlin Patterson <merlinpatt@gmail.com>
2018-10-24 23:10:44 -0400

    Add options addon

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,5 +1,6 @@
 import { configure, addDecorator } from '@storybook/react';
-import { withKnobs, text, boolean, number } from '@storybook/addon-knobs/react';
+import { withKnobs } from '@storybook/addon-knobs/react';
+import { setOptions } from '@storybook/addon-options';
 
 const req = require.context('../src', true, /stories\.js$/);
 
@@ -8,5 +9,34 @@ function loadStories() {
 }
 
 addDecorator(withKnobs);
+
+setOptions({
+  // name to display in the top left corner
+  name: 'Pup - Clever Beagle',
+  // URL for name in top left corner to link to
+  url: 'https://cleverbeagle.com',
+  // show story component as full screen
+  goFullScreen: false,
+  // display panel that shows a list of stories
+  showStoriesPanel: true,
+  // display panel that shows addon configurations
+  showAddonPanel: true,
+  // display floating search box to search through stories
+  showSearchBox: false,
+  // show addon panel as a vertical panel on the right
+  addonPanelInRight: false,
+  // sorts stories
+  sortStoriesByKind: false,
+  // regex for finding the hierarchy separator
+  // @example: /\/|\./ - split by `/` or `.`
+  hierarchySeparator: null,
+  // regex for finding the hierarchy root separator
+  // @example: /\|/ - split by `|`
+  hierarchyRootSeparator: null,
+  // sidebar tree animations
+  sidebarAnimations: true,
+  // id to select an addon panel
+  selectedAddonPanel: undefined, // The order of addons in the "Addon panel" is the same as you import them in 'addons.js'. The first panel will be opened by default as you run Storybook
+});
 
 configure(loadStories, module);
