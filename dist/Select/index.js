ef08435ddcac386e28b3bd4242c3c36d10444252
Ryan Glover <ryan.glover@cleverbeagle.com>
2019-04-19 11:33:33 -0500

    fix tab style

--- dist/Select/index.js
+++ dist/Select/index.js
@@ -15,13 +15,10 @@ function _typeof(obj) { if (typeof Symbol === "function" && typeof Symbol.iterat
 
 function _extends() { _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; }; return _extends.apply(this, arguments); }
 
-<<<<<<< HEAD
 function _objectWithoutProperties(source, excluded) { if (source == null) return {}; var target = _objectWithoutPropertiesLoose(source, excluded); var key, i; if (Object.getOwnPropertySymbols) { var sourceSymbolKeys = Object.getOwnPropertySymbols(source); for (i = 0; i < sourceSymbolKeys.length; i++) { key = sourceSymbolKeys[i]; if (excluded.indexOf(key) >= 0) continue; if (!Object.prototype.propertyIsEnumerable.call(source, key)) continue; target[key] = source[key]; } } return target; }
 
 function _objectWithoutPropertiesLoose(source, excluded) { if (source == null) return {}; var target = {}; var sourceKeys = Object.keys(source); var key, i; for (i = 0; i < sourceKeys.length; i++) { key = sourceKeys[i]; if (excluded.indexOf(key) >= 0) continue; target[key] = source[key]; } return target; }
 
-=======
->>>>>>> 0569c6197e22ce96fcd40566c5e09af9b7228583
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
 function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
@@ -68,7 +65,6 @@ function (_React$Component) {
     value: function render() {
       var _this$props = this.props,
           innerRef = _this$props.innerRef,
-<<<<<<< HEAD
           children = _this$props.children,
           disabled = _this$props.disabled,
           rest = _objectWithoutProperties(_this$props, ["innerRef", "children", "disabled"]);
@@ -79,14 +75,6 @@ function (_React$Component) {
         ref: innerRef,
         disabled: disabled
       }, rest), children));
-=======
-          children = _this$props.children;
-      return _react.default.createElement("div", {
-        className: "fur-form-select"
-      }, _react.default.createElement("select", _extends({
-        ref: innerRef
-      }, this.props), children));
->>>>>>> 0569c6197e22ce96fcd40566c5e09af9b7228583
     }
   }]);
 
